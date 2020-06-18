4bed92558f9731164a366ba3048353c8e5d6c66f
Liam Hockley <xdega121@gmail.com>
2020-06-18 12:37:30 -0700

    Update custom loader

--- src/views/Blog.jsx
+++ src/views/Blog.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
 import parse from "html-react-parser";
 import api from "../api/article";
 
@@ -21,25 +22,39 @@ const Blog = () => {
   const blogItems = articles.map((item) => {
     const title = item.attributes.title;
     const body = item.attributes.body.value;
-    return <BlogItem title={title} body={body} key={item.id} />;
+    let date = new Date(item.attributes.created);
+    date = moment(date).format("DD MMMM YYYY");
+
+    return <BlogItem title={title} body={body} date={date} key={item.id} />;
   });
   if (loading) {
-    return <div className="flex-grow w-50">LOADING...</div>;
+    return (
+      <div className="flex-grow">
+        <div className="loader mt-3"></div>
+      </div>
+    );
   } else {
     return (
-      <div className="flex-grow --fade-in">
-        <h2>Blog</h2>
+      <main className="flex-grow mt-2 --fade-in">
+        <h2 className="sm:hidden text-dark-gray uppercase text-2xl font-header">
+          Blog
+        </h2>
         {blogItems}
-      </div>
+      </main>
     );
   }
 };
 
-const BlogItem = ({ title, body }) => {
+const BlogItem = ({ title, body, date }) => {
   return (
-    <article>
-      <h2>{title}</h2>
-      <div>{parse(body)}</div>
+    <article className="flex mt-2 mb-5">
+      <div className="flex-auto">
+        <div className="uppercase text-white bg-dark-gray py-1 px-2 flex justify-between flex-col sm:flex-row">
+          <span className="font-bold">{title}</span>
+          <span>{date}</span>
+        </div>
+        <div className="leading-tight mt-1 px-2">{parse(body)}</div>
+      </div>
     </article>
   );
 };
