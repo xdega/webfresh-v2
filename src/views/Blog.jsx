27f7c7d81de6483fe8342c1834e10fb41159d98a
Liam Hockley <xdega121@gmail.com>
2020-06-18 11:39:46 -0700

    Initial API link for Blog page.

--- src/views/Blog.jsx
+++ src/views/Blog.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../api/article";
 
-const Blog = ({ title }) => {
+const Blog = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    api.index().then((resp) => {
+      setArticles(resp.data.data);
+    });
+  }, []);
+
+  const blogItems = articles.map((item) => {
+    const title = item.attributes.title;
+    const body = item.attributes.body.value;
+    return <BlogItem title={title} body={body} key={item.id} />;
+  });
+
   return (
     <div className="flex-grow">
       <h2>Blog</h2>
-      <article className="">Blog Content Here...</article>
+      {blogItems}
     </div>
+  );
+};
+
+const BlogItem = ({ title, body }) => {
+  return (
+    <article>
+      <h2>{title}</h2>
+      <div>{body}</div>
+    </article>
   );
 };
 
