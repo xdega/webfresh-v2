a288fc9be6c1fd5ef78fe24fdbcaff6057ee7a98
Liam Hockley <xdega121@gmail.com>
2020-06-20 10:30:01 -0700

    Fix portfolio order

--- src/api/portfolio.js
+++ src/api/portfolio.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
   index() {
     return axios.get(
-      "https://api.webfresh.dev/jsonapi/node/portfolio_item?include=field_thumbnail&sort=created"
+      "https://api.webfresh.dev/jsonapi/node/portfolio_item?include=field_thumbnail&sort=-created"
     );
   },
 };
